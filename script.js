fc5880007e9e08cd288b1f58259be5a054de1a9d
nikoloz93 <tsogiaidze@yahoo.com>
2018-10-27 19:17:36 +0400

    Fixed hour numbers

--- script.js
+++ script.js
@@ -2,15 +2,42 @@
 
 	var currentTime = { hour : 0, minute : 0, second : 0 };
 
+	createHourLabels();
+
+	drawMarks();
+
 	setCurrentTime();
 
 	updateHoursMinutesSeconds();
 
+	setInterval(function(){
+		updateHoursMinutesSeconds();
+	}, 1000);
+
 	function updateHoursMinutesSeconds(){
 		setCurrentTime();
-		updateHours();
-		updateMinutes();
 		updateSeconds();
+		updateMinutes();
+		updateHours();
+	}
+
+	function degreesToRadians(degrees)
+	{
+	  return degrees * (Math.PI/180);
+	}
+
+	function getCoordsByDegree(theta){
+		var radius = 130;
+		return { x : radius * Math.sin(degreesToRadians(theta)), y : radius * Math.cos(degreesToRadians(theta)) }
+	}
+
+	function createHourLabels(){
+		var arr = [6,5,4,3,2,1,12,11,10,9,8,7];
+
+        d3.select(".numbers").selectAll("text").data(arr).enter().append("text")
+         	.attr("x", function(d,i) { console.log(d, getCoordsByDegree(i * 30).x + 175, getCoordsByDegree(i * 30).y + 175); return getCoordsByDegree(i * 30).x + 175 - 6 ; })
+         	.attr("y", function(d,i) { return getCoordsByDegree(i * 30).y + 175 + 5; })
+         	.text(function(d) { return d; });
 	}
 
 	function updateHours(){
@@ -28,9 +55,21 @@
 		d3.select(".second").attr("transform", "rotate("+degree+" 175 175)");
 	}
 
-	setInterval(function(){
-		updateHoursMinutesSeconds();
-	}, 1000);
+	function drawMarks(){
+		var clockMarks = [];
+
+		for (var i = 1; i <= 61; i++) {
+		   i % 5 != 0 ? clockMarks.push(i) : "";
+		}
+
+		d3.select(".marks").selectAll("text").data(clockMarks).enter()
+			.append("text")
+			.attr("x", "175")
+			.attr("y", "175")
+			.attr("dy", "-128")
+			.attr("transform", function(d) { var degree = d * 6; return "rotate("+degree+" 175 175)" })
+			.text("|");
+	}
 
 	function setCurrentTime(){
 		currentTime.hour = new Date().getHours();
